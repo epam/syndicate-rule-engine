7f7840faf95d545f498fade3c20005a930ec30ef
Dmytro Afanasiev <dmytro_afanasiev1@epam.com>
2024-05-14 16:14:40 +0300

    Add entrypoint script

---
+++ src/onprem/scripts/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Creating the necessary buckets in Minio"
+python main.py create_buckets
+
+echo "Creating indexes in MongoDB"
+python main.py create_indexes
+
+echo "Creating the necessary engine and token in Vault"
+python main.py init_vault
+
+echo "Initializing"
+python main.py init
+
+echo "Starting server"
+python main.py run --gunicorn --swagger
