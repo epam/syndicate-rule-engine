9c099079c90bb39d680e2e26e981cfbae63a6926
iothor <ihor_tkhoruk@epam.com>
2025-07-09 16:21:19 +0300

    fix ci/cd test fail

--- tests/tests_services/test_resource_collector.py
+++ tests/tests_services/test_resource_collector.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch, MagicMock
+
 from c7n.resources.resource_map import ResourceMap as AWSResourceMap
 from c7n_azure.resources.resource_map import ResourceMap as AzureResourceMap
 from c7n_gcp.resources.resource_map import ResourceMap as GCPResourceMap
@@ -10,13 +12,17 @@ def test_aws_policies():
     assert all(policy.resource_type.startswith('aws.') for policy in policies)
     assert all(policy.resource_type in AWSResourceMap for policy in policies)
 
-def test_azure_policies():
+@patch('c7n_azure.session.Session')
+def test_azure_policies(mock_session):
+    mock_session_instance = MagicMock()
+    mock_session.return_value = mock_session_instance
+    
     policies = ResourceCollector.get_azure_policies()
 
     assert all(policy.resource_type.startswith('azure.') for policy in policies)
     assert all(policy.resource_type in AzureResourceMap for policy in policies)
 
-def test_gcp_policies():
+def test_gcp_policies():    
     policies = ResourceCollector.get_gcp_policies()
 
     assert all(policy.resource_type.startswith('gcp.') for policy in policies)
