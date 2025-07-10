1534b5781cc72c939eed843b13b840b72b344e2c
iothor <ihor_tkhoruk@epam.com>
2025-07-10 09:08:08 +0300

    fix ci/cd test fail 2

--- tests/tests_services/test_resource_collector.py
+++ tests/tests_services/test_resource_collector.py
@@ -12,17 +12,20 @@ def test_aws_policies():
     assert all(policy.resource_type.startswith('aws.') for policy in policies)
     assert all(policy.resource_type in AWSResourceMap for policy in policies)
 
-@patch('c7n_azure.session.Session')
-def test_azure_policies(mock_session):
-    mock_session_instance = MagicMock()
-    mock_session.return_value = mock_session_instance
+@patch('c7n_azure.session.AzureCredential')
+@patch('azure.identity._credentials.azure_cli._run_command')
+def test_azure_policies(mock_run_command, mock_credential):
+    mock_run_command.return_value = '{"id": "test-subscription", "name": "Test Subscription"}'
+    
+    mock_credential_instance = MagicMock()
+    mock_credential.return_value = mock_credential_instance
     
     policies = ResourceCollector.get_azure_policies()
 
     assert all(policy.resource_type.startswith('azure.') for policy in policies)
     assert all(policy.resource_type in AzureResourceMap for policy in policies)
 
-def test_gcp_policies():    
+def test_gcp_policies():
     policies = ResourceCollector.get_gcp_policies()
 
     assert all(policy.resource_type.startswith('gcp.') for policy in policies)
