eea0178f7df7808aa3daef9220e18d78d67c931e
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2025-07-27 18:02:15 +0300

    Fix test

--- tests/tests_models/test_resource.py
+++ tests/tests_models/test_resource.py
@@ -18,18 +18,18 @@ class TestResource:
             }
         )
         
-        initial_hash = resource.hash
+        initial_hash = resource.sha256
         
         assert initial_hash is not None
         assert len(initial_hash) == 64
         
         modified_data = {"key1": "modified_value1", "key2": "value2"}
         resource.data = modified_data
-        modified_hash = resource.hash
+        modified_hash = resource.sha256
         
         assert modified_hash != initial_hash
         assert len(modified_hash) == 64
         
         resource.data = initial_data
-        restored_hash = resource.hash
+        restored_hash = resource.sha256
         assert restored_hash == initial_hash
--- tests/tests_services/test_resource_collector.py
+++ tests/tests_services/test_resource_collector.py
@@ -7,10 +7,11 @@ from c7n_gcp.resources.resource_map import ResourceMap as GCPResourceMap
 from services.resources_collector import CustodianResourceCollector
 
 def test_aws_policies():
-    policies = CustodianResourceCollector.get_aws_policies()
+    policies = CustodianResourceCollector.build().load_aws_policies()
 
     assert all(policy.resource_type.startswith('aws.') for policy in policies)
     assert all(policy.resource_type in AWSResourceMap for policy in policies)
+
 
 @patch('c7n_azure.session.AzureCredential')
 @patch('azure.identity._credentials.azure_cli._run_command')
@@ -20,13 +21,14 @@ def test_azure_policies(mock_run_command, mock_credential):
     mock_credential_instance = MagicMock()
     mock_credential.return_value = mock_credential_instance
     
-    policies = CustodianResourceCollector.get_azure_policies()
+    policies = CustodianResourceCollector.build().load_azure_policies()
 
     assert all(policy.resource_type.startswith('azure.') for policy in policies)
     assert all(policy.resource_type in AzureResourceMap for policy in policies)
 
+
 def test_gcp_policies():
-    policies = CustodianResourceCollector.get_gcp_policies()
+    policies = CustodianResourceCollector.build().load_google_policies()
 
     assert all(policy.resource_type.startswith('gcp.') for policy in policies)
     assert all(policy.resource_type in GCPResourceMap for policy in policies)
