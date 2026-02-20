0fe5a07c4f03ce566d315fe183dd9aeb72bfd0d7
DemianDiakulych <Demyandyak@ukr.net>
2026-02-20 09:57:33 +0200

    Added common contacts to the test Customer and Tenant

--- tests/conftest.py
+++ tests/conftest.py
@@ -204,7 +204,7 @@ def main_customer() -> 'Customer':
     return Customer(
         name='TEST_CUSTOMER',
         display_name='test customer',
-        admins=[],
+        admins=["admin@gmail.com"],
         is_active=True,
     )
 
@@ -223,7 +223,7 @@ def aws_tenant(main_customer: 'Customer') -> 'Tenant':
         customer_name=main_customer.name,
         cloud='AWS',
         project=AWS_ACCOUNT_ID,
-        contacts={},
+        contacts={"teanant_contact_1@gamil.com","teanant_contact_2@gamil.com"},
         activation_date=utc_iso(utc_datetime() - timedelta(days=30)),
         linked_to='TESTING',
         regions=[
