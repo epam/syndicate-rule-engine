ebf716950454aed2fd6d091c2d4a34007f4dbe91
Roman Myhun <roman_myhun@epam.com>
2026-03-09 16:43:49 +0200

    Refactor cloudtrail command to remove unused wrap_in_eventbridge option and streamline event processing logic. Update event command structure by removing the unimplemented eventbridge command.

--- cli/srecli/group/job_event.py
+++ cli/srecli/group/job_event.py
@@ -88,13 +88,9 @@ def event():
 @click.option('--event_name', '-en', type=str,
               default=['UpdateInstanceInformation'], show_default=True,
               help='CloudTrail event name to simulate', multiple=True)
-@click.option('--wrap_in_eventbridge', '-eb', is_flag=True,
-              help='Wraps CloudTrail event in EventBridge event with '
-                   'detail-type `AWS API Call via CloudTrail`')
 @cli_response()
 def cloudtrail(ctx: ContextObj, cloud_identifier: tuple, region: tuple,
-               event_source: tuple, event_name: tuple,
-               wrap_in_eventbridge: bool, customer_id):
+               event_source: tuple, event_name: tuple, customer_id):
     """
     Command to simulate event-driven request from CloudTrail-based
     event-listener. Use it just to check whether
@@ -104,49 +100,23 @@ def cloudtrail(ctx: ContextObj, cloud_identifier: tuple, region: tuple,
         list(cloud_identifier), list(region),
         list(event_source), list(event_name)
     )
-    if wrap_in_eventbridge:
-        _temp = events
-        events = []
-        for rec in _temp:
-            events.append(
-                _build_eventbridge_record(
-                    detail_type='AWS API Call via CloudTrail',
-                    source='aws.resource-groups',  # not important here
-                    account=cloud_identifier[0],
-                    region=region[0],
-                    detail=rec
-                )
+    _temp = events
+    events = []
+    for rec in _temp:
+        events.append(
+            _build_eventbridge_record(
+                detail_type='AWS API Call via CloudTrail',
+                source='aws.resource-groups',  # not important here
+                account=cloud_identifier[0],
+                region=region[0],
+                detail=rec
             )
+        )
     return ctx['api_client'].event_action(
         version='1.0.0',
         vendor='AWS',
         events=events,
         customer_id=customer_id
     )
-
-
-@event.command(cls=ViewCommand, name='eventbridge')
-@click.option('--account', '-cid', type=str, required=True,
-              multiple=True, help='Account id to build event payload with')
-@click.option('--region', '-r', type=str, default=['eu-central-1'],
-              multiple=True,
-              show_default=True, help='Region which the event came from')
-@click.option('--source', '-es', type=str, default=['aws.ec2'],
-              show_default=True, help='CloudTrail event source to simulate',
-              multiple=True)
-@click.option('--detail_type', '-dt', type=str,
-              default=['EC2 Instance State-change Notification'],
-              show_default=True,
-              help='CloudTrail event name to simulate', multiple=True)
-@cli_response()
-def eventbridge(ctx: ContextObj, account: tuple, region: tuple, source: tuple,
-                detail_type: tuple, customer_id):
-    """
-    Command to simulate event-driven request from EventBridge-based
-    event-listener. Use it just to check whether
-    event-driven jobs work properly
-    """
-    raise click.ClickException('Not implemented yet')
-
 
 event.add_command(maestro)
